ea9ebd496e6f4566bc9bea53921de53fcfe92a29
Taha <t2905j@gmail.com>
2021-01-12 21:03:49 +0530

    Comment Added

---
+++ randomNumGen.py
@@ -0,0 +1,5 @@
+#These lines of code can easily generate a random number all we need to do is input the upper bound
+#set function has the major role in this function which creates an unordered list of all the items in it.
+lowerBound = int(input())
+upperBound = int(input())
+print(set(map(str,range(lowerBound,upperBound))).pop()) 
